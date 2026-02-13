6b9d906b62c714d06aca1e5eede0838a1eabf9c0
nftraja <nftraja1@gmail.com>
2026-02-13 17:42:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-document.addEventListener('touchmove',function(e){
- if(e.scale && e.scale!==1){e.preventDefault();}
-},{passive:false});
-
-let lastTouchEnd=0;
-document.addEventListener('touchend',function(e){
- const now=Date.now();
- if(now-lastTouchEnd<=300){e.preventDefault();}
- lastTouchEnd=now;
-},false);
-
-const menuBtn=document.getElementById("menuBtn");
-const drawer=document.getElementById("drawer");
-const overlay=document.getElementById("overlay");
+const menuBtn = document.getElementById("menuBtn");
+const drawer = document.getElementById("drawer");
+const overlay = document.getElementById("overlay");
 
 if(menuBtn){
  menuBtn.addEventListener("click",()=>{
@@ -19,6 +8,7 @@ if(menuBtn){
   overlay.classList.toggle("active");
  });
 }
+
 if(overlay){
  overlay.addEventListener("click",()=>{
   drawer.classList.remove("active");
